ef1772b56d90748ad5978f9bc91e9797355724cf
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2024-08-05 22:35:00 +0400

    added get models information function

--- groq.js
+++ groq.js
@@ -4,9 +4,19 @@ dotenv.config();
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
+const getModels = async () => {
+  return await groq.models.list();
+};
+
+getModels().then((models) => {
+  console.log(models);
+});
+
 export async function main() {
   const chatCompletion = await getGroqChatCompletion();
   // Print the completion returned by the LLM.
+  console.log(chatCompletion);
+  console.log(chatCompletion.choices[0]?.message);
   console.log(chatCompletion.choices[0]?.message?.content || "");
 }
 
@@ -22,4 +32,4 @@ export async function getGroqChatCompletion() {
   });
 }
 
-main();
+// main();
